6bc29965d151b3687b24aaf3d7445ba2527e41a0
Ubuntu <ubuntu@ip-172-31-9-52.eu-west-1.compute.internal>
2024-05-22 18:03:40 +0000

    add GET /users/{user_id} endpoint to tp_1 api

--- FastApi/tp_1.py
+++ FastApi/tp_1.py
@@ -29,3 +29,8 @@ def get_index():
 @api.get('/users')
 def get_index():
     return users_db
+
+@api.get('/users/{user_id}')
+def get_index(user_id:int):
+    user = next((user for user in users_db if user['user_id'] == user_id), None)
+    return user if user is not None else []

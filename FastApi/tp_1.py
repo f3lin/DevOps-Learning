f30a0c168127f8043505cf088b57c3fde126ce01
Ubuntu <ubuntu@ip-172-31-9-52.eu-west-1.compute.internal>
2024-05-22 17:48:51 +0000

    add GET /users endpoint to tp_1 api

--- FastApi/tp_1.py
+++ FastApi/tp_1.py
@@ -2,6 +2,30 @@ from fastapi import FastAPI
 
 api = FastAPI()
 
+users_db = [
+    {
+        'user_id': 1,
+        'name': 'Alice',
+        'subscription': 'free tier'
+    },
+    {
+        'user_id': 2,
+        'name': 'Bob',
+        'subscription': 'premium tier'
+    },
+    {
+        'user_id': 3,
+        'name': 'Clementine',
+        'subscription': 'free tier'
+    }
+]
+
+
 @api.get('/')
 def get_index():
     return "Bienvenue sur mon API"
+
+
+@api.get('/users')
+def get_index():
+    return users_db

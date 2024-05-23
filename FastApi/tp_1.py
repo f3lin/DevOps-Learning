daa65cd9ef68d38bb1faf5d9618ef21d070fc00e
Ubuntu <ubuntu@ip-172-31-9-52.eu-west-1.compute.internal>
2024-05-23 23:10:42 +0000

    add some http response code

--- FastApi/tp_1.py
+++ FastApi/tp_1.py
@@ -14,35 +14,50 @@ users_db = [
         User(user_id=3, name='Clementine', subscription='free tier')
     ]
 
+responses = {
+    200: {"description": "OK"},
+    400: {"description": "User already exists"},
+    404: {"description": "User not found"},
+    403: {"description": "Not enough privileges"},
+}
+
 
 @api.get('/')
 def get_index():
     return "Bienvenue sur mon API"
 
 
-@api.get('/users')
+@api.get('/users', responses=responses)
 def get_index():
     return users_db
 
-@api.get('/users/{user_id}')
-def get_index(user_id:int):
-    user = next((user for user in users_db if user['user_id'] == user_id), None)
-    return user if user is not None else []
+# @api.get('/users/{user_id}', responses=responses)
+# def get_index(user_id:int):
+#     user = next((user for user in users_db if user['user_id'] == user_id), None)
+#     return user if user is not None else []
 
-@api.get('/users/{user_id}/name')
-def get_index(user_id:int):
-    user = next((user for user in users_db if user['user_id'] == user_id), None)
-    return {'name': user['name']} if user is not None else []
+@api.get('/users/{user_id}', responses=responses)
+def get_user(user_id: int):
+    user = next((user for user in users_db if user.user_id == user_id), None)
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
 
-@api.post('/users')
+# @api.get('/users/{user_id}/name', responses=responses)
+# def get_index(user_id:int):
+#     user = next((user for user in users_db if user['user_id'] == user_id), None)
+#     return {'name': user['name']} if user is not None else []
+
+
+
+@api.post('/users', responses=responses)
 def create_user(user:User):
     if any(u.user_id == user.user_id for u in users_db):
         raise HTTPException(status_code=400, detail="User already exists")
-    else:
-        users_db.append(user)
-        return user
+    users_db.append(user)
+    return user
     
-@api.put('/users/{user_id}')
+@api.put('/users/{user_id}', responses=responses)
 def update_user(user_id:int, user:User):
     for idx, u in enumerate(users_db):
             if u.user_id == user_id:
@@ -51,7 +66,7 @@ def update_user(user_id:int, user:User):
     raise HTTPException(status_code=404, detail="User not found")
 
 
-@api.delete('/users/{user_id}')
+@api.delete('/users/{user_id}', responses=responses)
 def delete_user(user_id: int):
     for idx, u in enumerate(users_db):
         if u.user_id == user_id:
